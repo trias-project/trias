e3734f7495bb6e914eba8f2bea040403d0609554
damianooldoni <damiano.oldoni@inbo.be>
2017-11-30 12:07:50 +0100

    Simplify download ocurrences process and change to tsv

--- R/update_download_list.R
+++ R/update_download_list.R
@@ -7,31 +7,30 @@
 #' @param file file containing all occurrence downloads from GBIF
 #' @param download_to_add GBIF download key to be added in database
 #' @param countries vector with countries
+#' @param input_checklist input file with species
 #' @return message with the performed updates
 #'
 #' @export
 #' @importFrom rgbif occ_download_meta
 update_download_list <- function(file, download_to_add, input_checklist, countries) {
   
-  downloads <- read.table(file = file, header = TRUE, sep = ",", stringsAsFactors = FALSE)
-  
+  downloads <- read.table(file = file, header = TRUE, sep = "\t", stringsAsFactors = FALSE)
   # downloadKey not present
   if (is.element(download_to_add, downloads$gbif_download_key) == FALSE) {
     metadata <- occ_download_meta(key = download_to_add)
     gbif_download_status <- metadata$status
     gbif_download_doi <- metadata$doi
     gbif_download_created <- metadata$created
-    write.table(x = list(toString(download_to_add), input_checklist, countries, 
+    write.table(x = list(toString(download_to_add), input_checklist, toString(countries$country), 
                          gbif_download_created, gbif_download_status, gbif_download_doi), file = file,
-                append = TRUE, sep = ",", quote = FALSE, row.names = FALSE, col.names =! file.exists(file))
+                append = TRUE, sep = "\t", quote = FALSE, row.names = FALSE, col.names =! file.exists(file))
     print(paste0("gbif_download_Key ", download_to_add, 
                  " added to ", file, "; download status = ", gbif_download_status))
-    #reload file
-    downloads <- read.table(file, header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    # reload file
+    downloads <- read.table(file, header = TRUE, sep = "\t", stringsAsFactors = FALSE)
   } else {
     print(paste0("gbif_download_Key ", download_to_add, " already present in ", file))
     }
-  
   # check all downloads with status "PREPARING" or "RUNNING".
   changes <- FALSE
   for (i in 1:length(downloads)) {
@@ -51,12 +50,10 @@ update_download_list <- function(file, download_to_add, input_checklist, countri
       }
     }
   }
-  
   # if download status changes are detected, then csv is rewritten with the needed changes  
   if (!changes) {
     print("No changes in download status detected")
   } else {
-    write.table(x = downloads, file = file, sep = ",", quote = FALSE, row.names = FALSE)
+    write.table(x = downloads, file = file, sep = "\t", quote = FALSE, row.names = FALSE)
   }
-  
 }
--- tests/testthat/test-query_for_occ_download.R
+++
@@ -1,5 +0,0 @@
-context("query_for_occ_download")
-
-test_that("query via jsonlite", {
-  expect_equal(2 * 2, 4)
-})
