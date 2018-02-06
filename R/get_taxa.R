747194afa8af4b3824db5e937fda7f0670484069
damianooldoni <damiano.oldoni@inbo.be>
2018-02-06 09:54:52 +0100

    check input arguments - part 2
    Added assert() about checklist_keys or taxon_keys as tibble dataframes because this can be the case of using: `name_usage(..; return = data)`

--- R/get_taxa.R
+++ R/get_taxa.R
@@ -16,49 +16,84 @@ get_taxa <- function(
   taxon_keys = NULL,
   checklist_keys = NULL,
   limit = NULL) {
+  API_terms = c("key", "nubKey", "nameKey", "taxonID", "sourceTaxonKey",
+                "kingdom", "phylum", "order", "family", "genus", "species",
+                "kingdomKey", "phylumKey", "classKey", "orderKey", "familyKey",
+                "genusKey", "speciesKey", "datasetKey", "constituentKey",
+                "parentKey", "parent", "basionymKey", "basionym", 
+                "scientificName", "canonicalName", "authorship","nameType",
+                "rank", "origin", "taxonomicStatus", "nomenclaturalStatus", 
+                "remarks", "publishedIn", "numDescendants", "lastCrawled",
+                "lastInterpreted","issues", "synonym", "class")
   
   # test incoming arguments
   assertthat::assert_that(!all(!is.null(taxon_keys), !is.null(checklist_keys)),
               msg = paste("Both taxon_keys and checklist_keys not NULL.", 
                           "You should choose one of the two!", sep = " "))
+  
   # test parameter taxon_keys
   if (!is.null(taxon_keys)) {
     assertthat::assert_that(is.numeric(taxon_keys) | is.character(taxon_keys) |
-                               inherits(taxon_keys, "list"),
-              msg = "taxon_keys should be a numeric, character, vector or a list.")
+                              inherits(taxon_keys, "list") | 
+                              inherits(taxon_keys, "tbl_df"),
+              msg = paste("taxon_keys should be a numeric, character, vector,",
+                          "tibble data.frame or a list.", sep = " "))
   
     if (inherits(taxon_keys, "list")) {
       assertthat::assert_that(isTRUE(all.equal(names(taxon_keys), c("meta", "data"))),
-                  msg = "List is corrupted: meta and data are both expected and nothing else.")
-      assertthat::assert_that(isTRUE(all.equal(class(taxon_keys$data), c("tbl_df","tbl","data.frame"))),
-                  msg = paste("List is corrupted: class(data) doesn't match class", 
-                              "of data from name_usage(), tibble.", sep = " "))
+                  msg = paste("List is corrupted: meta and data are both",
+                              "expected and nothing else.", sep = " "))
+      assertthat::assert_that(isTRUE(all.equal(class(taxon_keys$data), 
+                                               c("tbl_df","tbl","data.frame"))),
+                  msg = paste("List is corrupted: class(data) !=", 
+                              "class(name_usage(key = ...)), tibble.", sep = " "))
       gbif_terms <- colnames(taxon_keys$data)
-      API_terms = c("key", "nubKey", "nameKey", "taxonID", "sourceTaxonKey",
-                    "kingdom", "phylum", "order", "family", "genus", "species",
-                    "kingdomKey", "phylumKey", "classKey", "orderKey", "familyKey",
-                    "genusKey", "speciesKey", "datasetKey", "constituentKey",
-                    "parentKey", "parent", "basionymKey", "basionym", 
-                    "scientificName", "canonicalName", "authorship","nameType",
-                    "rank", "origin", "taxonomicStatus", "nomenclaturalStatus", 
-                    "remarks", "publishedIn", "numDescendants", "lastCrawled",
-                    "lastInterpreted","issues", "synonym", "class")
       assertthat::assert_that(isTRUE(all(gbif_terms %in% API_terms)),
-                              msg = "One or more ttributes don't match API terms.")
+                              msg = "One or more attributes don't match API terms.")
+    }
+    
+    if (inherits(taxon_keys, "tbl_df")) {
+      gbif_terms <- colnames(taxon_keys)
+      assertthat::assert_that(isTRUE(all(gbif_terms %in% API_terms)),
+                              msg = "One or more attributes don't match API terms.")
     }
   }
   
+  # test parameter checklist_keys
   if (!is.null(checklist_keys)) {
-    assertthat::assert_that(is.character(checklist_keys) | 
-                               inherits(checklist_keys, "list"),
+    assertthat::assert_that(is.character(checklist_keys) |
+                              inherits(checklist_keys, "list") | 
+                              inherits(checklist_keys, "tbl_df"),
                           msg = paste("checklist_keys should be a character,",
-                                      "vector or a list.", sep = " "))
+                                      "vector, tibble data.frame or a list.", 
+                                      sep = " "))
     if (inherits(checklist_keys, "list")) {
       assertthat::assert_that(isTRUE(all.equal(names(checklist_keys), 
                                                c("meta", "data"))),
                               msg = paste("List is corrupted: meta and data are",
                                           "both expected and nothing else.", 
                                           sep = " "))
-      }
+      assertthat::assert_that(isTRUE(all.equal(class(checklist_keys$data), 
+                                               c("tbl_df","tbl","data.frame"))),
+                  msg = paste("List is corrupted: class(checklist_keys$data) !=", 
+                              "class(name_usage(datasetKey = ...)), tibble.", 
+                              sep = " "))
+      gbif_terms <- colnames(checklist_keys$data)
+      assertthat::assert_that(isTRUE(all(gbif_terms %in% API_terms)),
+                              msg = "One or more attributes don't match API terms.")
+    }
+    
+    if (inherits(checklist_keys, "tbl_df")) {
+      gbif_terms <- colnames(checklist_keys)
+      assertthat::assert_that(isTRUE(all(gbif_terms %in% API_terms)),
+                              msg = "One or more attributes don't match API terms.")
+    }
+    
+  }
+  
+  if (!is.null(limit)) {
+    assertthat::assert_that(is.numeric(class(limit)),
+                            msg = "Limit has to be numeric.")
+  }
   }
 }
--- tests/testthat/test_input_get_taxa.R
+++ tests/testthat/test_input_get_taxa.R
@@ -16,12 +16,7 @@ testthat::test_that("taxon_keys and checklist_keys cannot be both not NULL", {
                      "You should choose one of the two!", sep = " "))
 })
 
-testthat::test_that("taxon_keys class: numeric, character, vector or list.", {
-  expect_error(get_taxa(data.frame()), 
-               "taxon_keys should be a numeric, character, vector or a list.")
-})
-
-testthat::test_that("taxon_keys is a list: output of name_usage(key = key)", {
+testthat::test_that("taxon_keys is a list: output of name_usage(key = ...)", {
   expect_error(get_taxa(list(2)),
                "List is corrupted: meta and data are both expected and nothing else.")
   expect_error(get_taxa(list(meta = data.frame())),
@@ -29,28 +24,55 @@ testthat::test_that("taxon_keys is a list: output of name_usage(key = key)", {
   expect_error(get_taxa(list(data = data.frame())),
                "List is corrupted: meta and data are both expected and nothing else.")
   expect_error(get_taxa(list(meta = data.frame(),
-                             data = data.frame())),
-               paste("List is corrupted: class(data) doesn't match class", 
-                     "of data from name_usage(), tibble.", sep = " "))
-  expect_error(get_taxa(list(meta = data.frame(),
                              data = tibble::tibble(key = 4, batman = "batman"))),
-               "One or more ttributes don't match API terms.")
+               "One or more attributes don't match API terms.")
   })
+
+testthat::test_that(paste("taxon_keys is a tibble data.frame: output of",
+                          "name_usage(key = ...)$data", sep = " "), {
+  expect_error(get_taxa(taxon_keys = tibble::tibble(key = 1, batman = "batman")),
+    "One or more attributes don't match API terms.")
+  })
+
 
 testthat::test_that("checklist_keys class: character, (character) vector or list", {
-  expect_error(get_taxa(checklist_keys = 1),
-               "checklist_keys should be a numeric, character, vector or a list.")
-  # expect_error(get_taxa(checklist_keys = data.frame()),
-  #              s)
+  expect_error(get_taxa(checklist_keys = data.frame()),
+               paste("checklist_keys should be a character, vector,", 
+                     "tibble data.frame or a list."))
   })
 
-# if (inherits(taxon_keys, "list")) {
-#   assert_that(isTRUE(all.equal(names(taxon_keys), c("meta", "data"))),
-#               msg = "List is corrupted: meta and data are both expected and nothing else.")
-  
-# 
-# test_that("str_length of missing is missing", {
-#   expect_equal(str_length(NA), NA_integer_)
-#   expect_equal(str_length(c(NA, 1)), c(NA, 1))
-#   expect_equal(str_length("NA"), 2)
-# })
+testthat::test_that(
+  paste("checklist_keys is a list:",
+        "output of name_usage(datasetkey = datasetKey)", 
+        sep = " "), {
+  expect_error(get_taxa(checklist_keys = list(2)),
+               "List is corrupted: meta and data are both expected and nothing else.")
+  expect_error(get_taxa(checklist_keys =  list(meta = data.frame())),
+               "List is corrupted: meta and data are both expected and nothing else.")
+  expect_error(get_taxa(checklist_keys =  list(data = data.frame())),
+               "List is corrupted: meta and data are both expected and nothing else.")
+  expect_error(get_taxa(checklist_keys =  list(meta = data.frame(),
+                                               data = data.frame())),
+               paste("List is corrupted: class(checklist_keys$data) !=", 
+                     "class(name_usage(datasetKey = ...)), tibble.", 
+                     sep = " "))
+  expect_error(get_taxa(
+    checklist_keys =  list(meta = data.frame(),
+                           data = tibble::tibble(key = 4, batman = "batman"))),
+               "One or more attributes don't match API terms.")
+})
+
+testthat::test_that(paste("Checklist_keys is a tibble dataframe",
+                          "output of name_usage(datasetkey = datasetKey,",
+                          "return = data)",
+                          sep = " "), {
+  expect_error(get_taxa(
+    checklist_keys =  tibble::tibble(key = 1, batman = "batman")),
+    "One or more attributes don't match API terms.")
+})
+
+testthat::test_that("Limit is a number", {
+  expect_error(get_taxa(checklist_keys = "9ff7d317-609b-4c08-bd86-3bc404b77c42",
+                        limit = "10"),
+               "Limit has to be numeric.")
+  })
