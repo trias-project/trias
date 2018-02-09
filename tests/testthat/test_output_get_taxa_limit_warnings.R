4565acefaeca8a00cbbd0c2a60d14791aa8683c4
damianooldoni <damiano.oldoni@inbo.be>
2018-02-09 10:29:09 +0100

    add a test on limit warning (with chekclists as vector)

--- tests/testthat/test_output_get_taxa_limit_warnings.R
+++ tests/testthat/test_output_get_taxa_limit_warnings.R
@@ -6,6 +6,13 @@ testthat::test_that("checklist is character, limit higher than n. of records", {
                  "Dataset contains less records than limit.")
 })
 
+testthat::test_that("checklist is vector, limit higher than n. of records", {
+  expect_warning(get_taxa(
+    checklist_keys = c("46261ec5-38e8-44c9-b8e9-edaddf99fa29",
+                     "e4746398-f7c4-47a1-a474-ae80a4f18e92"),
+  limit = 5000))
+})
+
 testthat::test_that("taxon_key is vector, limit higher than n. of records", {
   expect_warning(get_taxa(taxon_keys =  c(1,2,3,4,5),
                           limit = 500),
