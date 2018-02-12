25fb9d27acd88713bed351c9b22302fce8fbcf7d
damianooldoni <damiano.oldoni@inbo.be>
2018-02-12 19:44:44 +0100

    Divide tests of has_distribution: testing the output and testing the input

--- tests/testthat/test_input_has_distribution.R
+++ tests/testthat/test_input_has_distribution.R
@@ -1,12 +1,9 @@
 context("input_has_distribution")
 
-testthat::test_that("distribution properties", {
+testthat::test_that("distribution properties: character or vectors", {
   expect_error(has_distribution("134086893", i_am_not_a_distribution_prop = "nooo"),
-               "Strings must match column names. Unknown columns: i_am_not_a_distribution_prop")
-  #countryCode instead of country
+               paste("Strings must match column names. Unknown columns:",
+                     "i_am_not_a_distribution_prop"))
   expect_error(has_distribution(134086954, countryCode = "BE"),
                "Strings must match column names. Unknown columns: countryCode")
-  expect_true(has_distribution(134086954, country = "BE"))
-  expect_false(has_distribution(134086954, country = "BE", status = "DOUBTFUL"))
-  expect_true(has_distribution(134086954, country = "BE", status = "PRESENT"))
 })
