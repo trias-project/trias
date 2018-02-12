2143d43a25cc8a76b32345532a350ec00a743517
damianooldoni <damiano.oldoni@inbo.be>
2018-02-12 16:14:39 +0100

    test has_distribution
    First tests written and succesfully done.

---
+++ tests/testthat/test_input_has_distribution.R
@@ -0,0 +1,12 @@
+context("input_has_synonym")
+
+testthat::test_that("distribution properties", {
+  expect_error(has_distribution("134086893", i_am_not_a_distribution_prop = "nooo"),
+               "Strings must match column names. Unknown columns: i_am_not_a_distribution_prop")
+  #countryCode instead of country
+  expect_error(has_distribution(134086954, countryCode = "BE"),
+               "Strings must match column names. Unknown columns: countryCode")
+  expect_true(has_distribution(134086954, country = "BE"))
+  expect_false(has_distribution(134086954, country = "BE", status = "DOUBTFUL"))
+  expect_true(has_distribution(134086954, country = "BE", status = "PRESENT"))
+})
