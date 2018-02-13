abf0e81d42ed06c0473b3c331ee2e33fd0318a47
damianooldoni <damiano.oldoni@inbo.be>
2018-02-13 14:08:09 +0100

    update has_distribution input parameters and behaviour
    update function behavior with alternative user distribution properties: status <-> occurrenceStatus, country <-> countryCode.
    Improve assertion errors and warnings

--- R/has_distribution.R
+++ R/has_distribution.R
@@ -26,9 +26,52 @@
 #' @importFrom stringr str_split
 has_distribution <- function(taxon_key, ...) {
   # df with all possible combinations of user's distribution properties values
-  user_properties <- list(...) %>% purrr::cross_df()
+  GBIF_distr_terms <- c("country", 
+                        "status","establishmentMeans")
+  user_properties <- list(...) 
+  
+  assertthat::assert_that(all(length(names(user_properties)) == 
+                                length(unique(names(user_properties)))),
+                          msg = paste("Duplicates in property assignments.", 
+                                      "Use vectors for multiple assignments."))
+  
+  # remove duplicates (country <-> countryStatus status <-> occurrenceStatus)
+  if (all(c("country", "countryCode") %in% names(user_properties))) {
+    warning(paste("country and countryCode are alternative names of the", 
+                  "same property. countryCode removed."))
+    user_properties$countryCode <- NULL
+  }
+  if (all(c("status", "occurrenceStatus") %in% names(user_properties))) {
+    warning(paste("status and occurrenceStatus are alternative names of the", 
+                  "same property. occurrenceStatus removed."))
+    user_properties$occurrenceStatus <- NULL
+  }
+  
+  # accept but rename the alternative name countryCode and occurrenceStatus
+  if ("countryCode" %in% names(user_properties)) {
+    warning("countryCode renamed to country")
+    names(user_properties) <- replace(names(user_properties), 
+                                      names(user_properties) == "countryCode", 
+                                      "country")
+  }
+  
+  if ("occurrenceStatus" %in% names(user_properties)) {
+    warning("occurrenceStatus renamed to status")
+    names(user_properties) <- replace(names(user_properties), 
+                                      names(user_properties) == "occurrenceStatus", 
+                                      "status")
+  }
+  
+  assertthat::assert_that(
+    all(names(user_properties) %in% GBIF_distr_terms),
+    msg = "Invalid distribution properties.")
+  
+  # create all possible combinations of distribution properties
+  user_properties %<>% purrr::cross_df()
   # set all characters uppercase
   user_properties %<>% dplyr::mutate_all(funs(toupper))
+  
+  # retrieve distribution properties from GBIF
   distr_properties <- rgbif::name_usage(key = as.integer(taxon_key),
                                         return = "data",
                                         data = "distribution") %>% 
@@ -39,6 +82,5 @@ has_distribution <- function(taxon_key, ...) {
     purrr::map(~stringr::str_split(., pattern = ",")) %>% 
     unlist(recursive = FALSE) %>% expand.grid() %>% 
     set_colnames(names(distr_properties)) %>% purrr::map_df(~as.character(.))
-  
   return(dplyr::intersect(user_properties, distr_properties) %>% nrow > 0)
 }
