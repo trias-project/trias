385805df477c6a1022d770349a57f6d9c22983dc
damianooldoni <damiano.oldoni@inbo.be>
2018-02-15 15:06:55 +0100

    typo in Roxygen odcumentation

--- tests/testthat/test_output_has_distribution.R
+++ tests/testthat/test_output_has_distribution.R
@@ -1,6 +1,6 @@
 context("output_has_distribution")
 
-testthat::test_that("testing output of has_distribution", {
+testthat::test_that("has_distribution with user parameters", {
 expect_true(has_distribution(134086954, country = "BE"))
 expect_false(has_distribution(134086954, country = "BE", status = "DOUBTFUL"))
 expect_false(has_distribution(134086954, country = "BE", status = "DOUBTFUL"))
