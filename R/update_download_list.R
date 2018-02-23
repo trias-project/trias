1873e6b860ff1bec27b2c9d4cfcefc8990256a56
damianooldoni <damiano.oldoni@inbo.be>
2018-02-23 23:38:09 +0100

    improve readibility
    Simplify code, split on more lines

--- R/update_download_list.R
+++ R/update_download_list.R
@@ -10,51 +10,54 @@
 #' @param input_checklist file with taxon keys used for that download
 #' @param countries vector with country codes used for that download
 #' @return message with the performed updates
-#'
 #' @export
 #' @importFrom rgbif occ_download_meta
 #' @importFrom readr read_delim
-update_download_list <- function(file, download_to_add, input_checklist, countries) {
-  downloads <- readr::read_delim(file, "\t", escape_double = FALSE, trim_ws = TRUE)
+#' @importFrom stringr str_c
+update_download_list <- function(file, download_to_add, input_checklist, 
+                                 url_doi_base = "") {
+  downloads <- readr::read_delim(file, "\t", 
+                                 escape_double = FALSE, trim_ws = TRUE)
   # downloadKey not present
   if (is.element(download_to_add, downloads$gbif_download_key) == FALSE) {
     metadata <- rgbif::occ_download_meta(key = download_to_add)
     gbif_download_status <- metadata$status
-    gbif_download_doi <- metadata$doi
+    gbif_download_doi <- stringr::str_c(url_doi_base,metadata$doi)
     gbif_download_created <- metadata$created
     write.table(x = list(toString(download_to_add), input_checklist, toString(countries$country),
                          gbif_download_created, gbif_download_status, gbif_download_doi), file = file,
                 append = TRUE, sep = "\t", quote = FALSE, row.names = FALSE, col.names =! file.exists(file))
-    print(paste0("gbif_download_Key ", download_to_add,
-                 " added to ", file, "; download status = ", gbif_download_status))
+    print(paste("gbif_download_Key", download_to_add,
+                 "added to", file, "; download status =", gbif_download_status))
     # reload file
     downloads <- read.table(file, header = TRUE, sep = "\t", stringsAsFactors = FALSE)
   } else {
-    print(paste0("gbif_download_Key ", download_to_add, " already present in ", file))
+    print(paste("gbif_download_Key", download_to_add, "already present in", file))
   }
   # check all downloads with status "PREPARING" or "RUNNING".
   changes <- FALSE
   for (i in 1:nrow(downloads)) {
-    if (is.element(downloads$gbif_download_status[i], c("RUNNING","PREPARING"))) {
+    if (downloads$gbif_download_status[i] %in% c("RUNNING","PREPARING")) {
       metadata <- rgbif::occ_download_meta(key = downloads$gbif_download_key[i])
       gbif_download_status <- metadata$status
-      # In case status SUCCEEDED or FAILED, update it
-      if (is.element(gbif_download_status, c("SUCCEEDED","FAILED"))) {
-        if (is.element(gbif_download_status, c("SUCCEEDED"))) {
+      # status is SUCCEEDED or FAILED
+      if (gbif_download_status %in% c("SUCCEEDED","FAILED")) {
+        if (gbif_download_status == "SUCCEEDED") {
           downloads$gbif_download_status[i] <- "SUCCEEDED"
         } else {
           downloads$gbif_download_status[i] <- "FAILED"
         }
-        print(paste0("gbif_download_Key ",
-                     downloads$gbif_download_key[i], ", status: ", gbif_download_status))
+        print(paste("gbif_download_Key", downloads$gbif_download_key[i], 
+                     ", status:", gbif_download_status))
         changes <- TRUE
       }
     }
   }
-  # if download status changes are detected, then csv is rewritten with the needed changes
+  # if download status changes detected, rewrite csv with changes
   if (!changes) {
     print("No changes in download status detected")
   } else {
-    write.table(x = downloads, file = file, sep = "\t", quote = FALSE, row.names = FALSE)
+    write.table(x = downloads, file = file, sep = "\t", 
+                quote = FALSE, row.names = FALSE)
   }
 }
