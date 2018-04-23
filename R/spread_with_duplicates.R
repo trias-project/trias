a547e880f7aa8d1120ead538e1ee750be59e8795
damianooldoni <damiano.oldoni@inbo.be>
2018-04-23 12:12:56 +0200

    Add function spread_with_duplicates
    This function helps creating a tidy representation of descriptions related to taxa

---
+++ R/spread_with_duplicates.R
@@ -0,0 +1,35 @@
+#' Spread a key-value pair across multiple columns in presence of duplicates
+#' 
+#' @param .data A dataframe
+#' @param key,value Column names or positions
+#' @param fill If set, missing values will be replaced with this value. 
+#' @examples 
+#' test1 <- data.frame(taxonKey = c(1, 1, 1, 1),	
+#'                 key = c("A", "B", "C", "C"),
+#'                 value = c("R", "S", "T", "X"),
+#'                 stringsAsFactors = FALSE)
+#' test1 %>% spread_with_duplicates(key, value, by = "taxonKey")
+#' # with NAs
+#' test2 <- data.frame(taxonKey = c(1, 1, 1, 2),	
+#'                 key = c("A", C", "C", "A"),
+#'                 value = c("R", "T", "X", "R"),
+#'                 stringsAsFactors = FALSE)
+#' test2 %>% spread_with_duplicates(key, value, by = "taxonKey")
+#' test2 %>% spread_with_duplicates(key, value, by = "taxonKey", fill = 0)
+spread_with_duplicates <- function(.df, key, value, by, fill = NA){
+  key_var <- tidyselect::vars_pull(names(.df), !! enquo(key))
+  col <- .df[key_var] %>% pull() %>% unique()
+  .df <- purrr::map(col, function(x) .df %>% filter(key == x)) %>%
+    purrr::map2(col, ~ change_colname(.x, .y, value)) %>%
+    purrr::reduce(dplyr::full_join, by = by)
+  if (!is.na(fill)){
+    .df <- .df %>% mutate_all(funs(replace(., is.na(.), fill)))
+  }
+  return(.df)
+}
+
+change_colname <- function(.df, new_colname, value){
+  .df %>% 
+    rename(!!new_colname := value) %>%
+    select(-key)
+}
