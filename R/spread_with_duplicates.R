d0c7acbff519d46423747bc3e25c93df9e84e0b9
damianooldoni <damiano.oldoni@inbo.be>
2018-04-25 13:56:38 +0200

    Add aggregate function to spread_with_duplicates
    As suggested by @stijnvanhoey , now our function has aggfunc input option.

--- R/spread_with_duplicates.R
+++ R/spread_with_duplicates.R
@@ -2,7 +2,9 @@
 #' 
 #' @param .data A dataframe
 #' @param key,value Column names or positions
-#' @param fill If set, missing values will be replaced with this value. 
+#' @param fill If set, missing values will be replaced with this value
+#' @param aggfunc function to aggregate key duplicates. 
+#' Default: NA (keep dulicates)
 #' @return A data.frame
 #' @examples 
 #' test1 <- data.frame(col1 = c(1, 1, 1, 1),	
@@ -13,19 +15,32 @@
 #' spread_with_duplicates(test1, key, value)
 #' spread_with_duplicates(test1, 3, 4)
 #' spread_with_duplicates(test1, -2, -1)
+#' 
 #' # with NAs
 #' test2 <- data.frame(col1 = c(1, 1, 1, 2),	
 #'                 key = c("A", "C", "C", "A"),
 #'                 value = c("R", "T", "X", "R"),
 #'                 stringsAsFactors = FALSE)
 #' spread_with_duplicates(test2, key, value)
-#' spread_with_duplicates(test2, key, value, fill = 0)
+#' spread_with_duplicates(test2, key, value, fill = "No idea")
+#' 
+#' # apply aggregate function
+#' test3 <- data.frame(col1 = c(1,1,1,1),
+#'   col2  = c("H", "H", "H", "H"),
+#'   key = c("A", "B", "C", "C"),
+#'   value = c(2, 3, 1, 8),
+#'   stringsAsFactors = FALSE)
+#' spread_with_duplicates(test1, key, value, aggfunc = str_c, collapse = "-")
+#' spread_with_duplicates(test2, key, value, aggfunc = min)
+#' spread_with_duplicates(test2, key, value, aggfunc = mean)
 #' @export
 #' @importFrom purrr map map2 reduce
 #' @importFrom dplyr mutate_all filter full_join pull
 #' @importFrom magrittr %>%
 #' @importFrom tidyselect vars_pull enquo
-spread_with_duplicates <- function(.data, key, value, fill = NA){
+spread_with_duplicates <- function(.data, key, value, fill = NA, 
+                                   aggfunc = NA, ...){
+  args = list(...)
   key_var <- vars_pull(names(.data), !! enquo(key))
   value_var <- vars_pull(names(.data), !! enquo(value))
   by = colnames(.data)[which(!colnames(.data) %in% c(key_var,value_var))]
@@ -34,7 +49,9 @@ spread_with_duplicates <- function(.data, key, value, fill = NA){
     unique()
   .data <- map(col, function(x) .data %>% filter(key == x)) %>%
     map2(col, ~ change_colname(.x, .y, value)) %>%
+    map2(col, ~ apply_aggfunc(.x, .y,  aggfunc = aggfunc, args)) %>%
     reduce(full_join, by = by)
+  
   if (!is.na(fill)){
     .data <- .data %>% mutate_all(funs(replace(., is.na(.), fill)))
   }
@@ -45,4 +62,15 @@ change_colname <- function(.data, new_colname, value){
   .data %>% 
     rename(!!new_colname := value) %>%
     select(-key)
+}
+
+apply_aggfunc <- function(.data, col_name,  aggfunc, args){
+  if (is.function(aggfunc)) {
+    args[["x"]] <- .data %>% pull(col_name)
+    aggregated_value <- do.call(aggfunc, args = args)
+    .data %>% mutate(!!col_name := aggregated_value) %>%
+      distinct()
+  } else {
+    .data
+  }
 }
