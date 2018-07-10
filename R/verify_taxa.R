01a5558b24e767045c3a99305e0b24993c139894
damianooldoni <damiano.oldoni@inbo.be>
2018-07-10 22:55:32 +0200

    Change verify_synonyms to verify_taxa
    Function input and output updated to new goal. Still checks against bugs and fine tuning needed. For example, what if the same checklist_scientificName occurs from another checklist? No new line should be added. Now there is no cehck for it. And no fuzzy match issue check as well: I think it is still not needed. All taxa with such issue have not nubKey (No match to GBIF backbone!)

--- R/verify_taxa.R
+++ R/verify_taxa.R
@@ -1,22 +1,19 @@
-#' Check and update verified synonyms
+#' Check and update verified taxa
 #'
-#' This function checks all verified synonyms, add new synonyms in order to be
-#' evaluated by an expert, update taxa names in case they have been changed and
-#' report the changes.
+#' This function checks all verified taxa, add new synonyms or taxa not matched
+#' to GBIF backbone in order to be evaluated by an expert, update taxa names in
+#' case they have been changed and report the changes.
 #' @param taxa: a dataframe with at least the following columns: \itemize{
-#'   \item{checklist_key} {: taxon key as provided by GBIF}
 #'   \item{checklist_scientificName} {: scientific name as provided by GBIF}
 #'   \item{checklist_datasetKey} {: dataset key (a UUID) of the checklist the
-#'   taxon comes from.} \item{nubKey} {: a GBIF backbone key.}
+#'   taxon comes from.} \item{backbone_taxonKey} {: a GBIF backbone key.}
 #'   \item{backbone_scientificName} {: scientific name as provided by GBIF
-#'   backbone.} \item{backbone_species} {: species name as provided by GBIF
-#'   backbone.} \item{backbone_speciesKey} {: a species key as provided by GBIF
 #'   backbone.} \item{backbone_taxonomicStatus} {taxonomic status as provided by
 #'   GBIF backbone.} \item{backbone_acceptedName} {: accepted name (in case of
 #'   synonyms) as provided by GBIF backbone.}  \item{backbone_acceptedKey} {:
 #'   accepted key as provided by GBIF backbone.} \item{backbone_issues} {:
 #'   issues as provided by GBIF backbone.} }
-#' @param verified_synonyms: a dataframe with at least the following columns:
+#' @param verified_taxa: a dataframe with at least the following columns:
 #'   \itemize{ \item{checklist_scientificName} \item{backbone_scientificName}
 #'   \item{backbone_acceptedName} \item{backbone_taxonKey}
 #'   \item{backbone_acceptedKey} \item{verified_key}{: to be populated manually
@@ -25,54 +22,78 @@
 #'   \item{date_added}{: to be populated by function} \item{issues}
 #'   \item{checklistst} {: checklists name where this taxa shows up},
 #'   \item{remarks} {: (optional) remarks as provided by the expert}}
-#' @return a list of five dataframes: \itemize{ \item{verified_synonyms}{: same
-#'   dataframe as input verified_synonyms, but now with updated info.}
-#'   \item{new_synonyms}{: a subset of verified_synonyms (same columns) with
-#'   added synonym relations (found in taxa, but not in verified_synonyms)}
-#'   \item{unused_synonyms}{: a subset of verified_synonyms (same columns) with
-#'   unused synonym relations (found in verified_synonyms, but not in taxa)}
+#' @return a list of five dataframes: \itemize{ \item{verified_taxa}{: same
+#'   dataframe as input verified_taxa, but now with updated info.}
+#'   \item{new_synonyms}{: a subset of verified_taxa (same columns) with
+#'   added synonym relations (found in taxa, but not in verified_taxa)}
+#'   \item{unused_synonyms}{: a subset of verified_taxa (same columns) with
+#'   unused synonym relations (found in verified_taxa, but not in taxa)}
 #'   \item{updated_scientificName}{: a dataframe with backbone_scientificName +
-#'   updated_backbone_scientificName} \item{updated_accepted}{: a dataframe with
-#'   backbone_accepted + updated_backbone_accepted} }
+#'   updated_backbone_scientificName} \item{updated_acceptedName}{: a dataframe
+#'   with backbone_accepted + updated_backbone_accepted} }
 #' @examples
 #' taxa_in <- data.frame(
-#'   backbone_taxonKey = c(2360181, 2427092, 2651108, 5228676),
+#'   checklist_scientificName = c("Aspius aspius",
+#'                                "Rana catesbeiana",
+#'                                "Polystichum tsus-simense J.Smith",
+#'                                "Apus apus (Linnaeus, 1758)",
+#'                                "Acmella spec."),
+#'   checklist_datasetKey = c("98940a79-2bf1-46e6-afd6-ba2e85a26f9f",
+#'                            "e4746398-f7c4-47a1-a474-ae80a4f18e92", 
+#'                            "9ff7d317-609b-4c08-bd86-3bc404b77c42",
+#'                            "39653f3e-8d6b-4a94-a202-859359c164c5",
+#'                            "9ff7d317-609b-4c08-bd86-3bc404b77c42"),
+#'   backbone_taxonKey = c(2360181, 2427092, 2651108, 5228676, NA),
 #'   backbone_scientificName = c("Aspius aspius (Linnaeus, 1758)",
 #'                               "Rana catesbeiana Shaw, 1802",
 #'                               "Polystichum tsus-simense (Hook.) J.Sm.",
-#'                               "Apus apus (Linnaeus, 1758)"),
-#'   backbone_acceptedKey = c(5851603, 2427091, 4046493, 5228676),
-#'   backbone_accepted = c("Leuciscus aspius (Linnaeus, 1758)",
-#'                        "Lithobates catesbeianus (Shaw, 1802)",
-#'                        "Polystichum luctuosum (Kunze) Moore.",
-#'                        "Apus apus (Linnaeus, 1758)"),
-#'   backbone_taxonomicStatus = c("SYNONYM", "SYNONYM", "SYNONYM", "ACCEPTED"),
+#'                               "Apus apus (Linnaeus, 1758)",
+#'                               NA),
+#'   backbone_kingdom = c("Animalia", "Animalia", "Plantae", "Plantae", NA),
+#'   backbone_taxonomicStatus = c("SYNONYM", "SYNONYM", "SYNONYM", 
+#'                                "ACCEPTED", NA),
+#'   backbone_acceptedKey = c(5851603, 2427091, 4046493, NA, NA),
+#'   backbone_acceptedName = c("Leuciscus aspius (Linnaeus, 1758)",
+#'                             "Lithobates catesbeianus (Shaw, 1802)",
+#'                             "Polystichum luctuosum (Kunze) Moore.",
+#'                             NA, 
+#'                             NA),
+#'   backbone_issues = c("ORIGINAL_NAME_DERIVED", NA, 
+#'                       "ORIGINAL_NAME_DERIVED", NA, NA),
 #'   stringsAsFactors = FALSE)
-#' verified_synonyms_in <- data.frame(
-#'  backbone_taxonKey = c(2427092,
-#'                         2651108,
-#'                         6723),
+#' verified_taxa_in <- data.frame(
+#'   checklist_scientificName = c("Rana catesbeiana",
+#'                                "Polystichum tsus-simense J.Smith",
+#'                                "Lemnaceae"),
 #'   backbone_scientificName = c("Rana catesbeiana Shaw, 1802",
 #'                               "Polystichum tsus-tsus-tsus (Hook.) Captain",
 #'                               "Lemnaceae"),
-#'   backbone_accepted = c("Lithobates dummyus (Batman, 2018)",
-#'                         "Polystichum luctuosum (Kunze) Moore.",
-#'                        "Araceae"),
-#'   backbone_acceptedKey = c(2427091,
-#'                            4046493,
-#'                            6979),
 #'   backbone_taxonomicStatus = c("SYNONYM", "SYNONYM", "SYNONYM"),
-#'   date_added = as.Date(c("2018-01-01",
-#'                          "2018-01-01",
-#'                          "2018-01-01")),
+#'   backbone_acceptedName = c("Lithobates dummyus (Batman, 2018)",
+#'                             "Polystichum luctuosum (Kunze) Moore.",
+#'                             "Araceae"),
+#'   backbone_taxonKey = c(2427092,
+#'                         2651108,
+#'                         6723),
+#'   backbone_acceptedKey = c(2427091,
+#'                           4046493,
+#'                           6979),
 #'   verified_key = c(2427091,
 #'                    4046493,
 #'                    6979),
+#'   backbone_kingdom = c("Animalia", "Plantae", "Plantae"),
+#'   date_added = as.Date(c("2018-07-01",
+#'                          "2018-07-01",
+#'                          "2018-07-01")),
+#'   backbone_issues = c("ORIGINAL_NAME_DERIVED", NA, "ORIGINAL_NAME_DERIVED"),
+#'   checklists = c("e4746398-f7c4-47a1-a474-ae80a4f18e92", 
+#'                  "9ff7d317-609b-4c08-bd86-3bc404b77c42",
+#'                  "e4746398-f7c4-47a1-a474-ae80a4f18e92"),
 #'   remarks = c("dummy example 1: backbone_accepted should be updated",
 #'               "dummy example 2: backbone_scientificName should be updated",
 #'               "dummy example 3: nothing should be changed"),
 #'   stringsAsFactors = FALSE)
-#' verify_synonyms(taxa = taxa_in, verified_synonyms = verified_synonyms_in)
+#' verify_taxa(taxa = taxa_in, verified_taxa = verified_taxa_in)
 #' @export
 #' @importFrom assertthat assert_that
 #' @importFrom rgbif name_usage
@@ -80,29 +101,49 @@
 #' @importFrom dplyr pull anti_join select left_join
 #' @importFrom magrittr %<>%
 #' @importFrom tibble as.tibble
-verify_synonyms <- function(taxa, verified_synonyms) {
+verify_taxa <- function(taxa, verified_taxa) {
   # test incoming arguments
-  name_col_taxa <- c("backbone_taxonKey", "backbone_scientificName",
-                "backbone_acceptedKey", "backbone_accepted",
-                "backbone_taxonomicStatus")
+  name_col_taxa <- c("checklist_scientificName", "checklist_datasetKey", 
+                     "backbone_taxonKey", "backbone_scientificName",
+                     "backbone_acceptedKey", "backbone_acceptedName",
+                     "backbone_taxonomicStatus", "backbone_issues")
   assert_that(is.data.frame(taxa))
   assert_that(all(name_col_taxa %in% names(taxa)))
   
-  name_col_synonyms <- c("backbone_taxonKey", "backbone_scientificName",
-                     "backbone_acceptedKey", "backbone_accepted",
-                     "backbone_taxonomicStatus", "date_added", "verified_key")
-  assert_that(is.data.frame(verified_synonyms))
-  assert_that(all(name_col_synonyms %in% names(verified_synonyms)))
+  name_col_verified <- c("checklist_scientificName", "backbone_scientificName",
+                         "backbone_taxonomicStatus", "backbone_acceptedName",
+                         "backbone_taxonKey", "backbone_acceptedKey", 
+                         "verified_key", "backbone_kingdom", "date_added", 
+                         "backbone_issues", "remarks", "checklists")
+  assert_that(is.data.frame(verified_taxa))
+  assert_that(all(name_col_verified %in% names(verified_taxa)))
   
   # find new synonyms
   new_synonyms <- taxa %>%
     filter(backbone_taxonomicStatus == "SYNONYM") %>%
-    filter(!backbone_taxonKey %in% verified_synonyms$backbone_taxonKey) %>% 
+    filter(!backbone_taxonKey %in% verified_taxa$backbone_taxonKey) %>% 
     rowwise() %>%
-    mutate(date_added = Sys.Date()) %>% ungroup()
-
+    mutate(date_added = Sys.Date(),
+           checklists = checklist_datasetKey) %>% 
+    ungroup() %>%
+    select(one_of(name_col_verified))
+  
+  # find new taxa not matched to GBIF backbone 
+  new_unmatches <- taxa %>%
+    filter(is.na(backbone_taxonKey)) %>%
+    filter(!checklist_scientificName %in% 
+             verified_taxa$checklist_scientificName) %>% 
+    rowwise() %>%
+    mutate(date_added = Sys.Date(),
+           checklists = checklist_datasetKey) %>% 
+    ungroup() %>%
+    select(one_of(name_col_verified))
+  
+  new_fuzzy_matches <- taxa %>% 
+    filter()
+  
   # create df of updated scientificNames 
-  updated_scientificName <- verified_synonyms %>%
+  updated_scientificName <- verified_taxa %>%
     filter(backbone_taxonKey %in% taxa$backbone_taxonKey) %>%
     anti_join(taxa, by = "backbone_scientificName") %>%
     select(backbone_scientificName, backbone_taxonKey) %>%
@@ -112,42 +153,50 @@ verify_synonyms <- function(taxa, verified_synonyms) {
     select(backbone_scientificName, updated_backbone_scientificName) %>% 
     as.tibble()
   
-  # create df of updated accepted
-  updated_accepted <- verified_synonyms %>%
+  # create df of updated acceptedName
+  updated_acceptedName <- verified_taxa %>%
     filter(backbone_taxonKey %in% taxa$backbone_taxonKey) %>%
-    anti_join(taxa, by = "backbone_accepted") %>%
-    select(backbone_accepted, backbone_taxonKey) %>%
+    filter(!is.na(backbone_acceptedName)) %>%
+    anti_join(taxa, by = "backbone_acceptedName") %>%
+    select(backbone_acceptedName, backbone_taxonKey) %>%
     left_join(taxa, by = "backbone_taxonKey") %>%
-    rename("backbone_accepted" = "backbone_accepted.x",
-           "updated_backbone_accepted" = "backbone_accepted.y") %>%
-    select(backbone_accepted, updated_backbone_accepted) %>% as.tibble()
+    rename("backbone_acceptedName" = "backbone_acceptedName.x",
+           "updated_backbone_acceptedName" = "backbone_acceptedName.y") %>%
+    select(backbone_acceptedName, updated_backbone_acceptedName) %>% as.tibble()
 
-  #update scientificName of verified synonyms
-  verified_synonyms <- verified_synonyms %>% 
+  #update scientificName of verified taxa
+  verified_taxa <- verified_taxa %>% 
     rowwise() %>%
     mutate(backbone_scientificName = ifelse(
       backbone_scientificName %in% updated_scientificName$backbone_scientificName,
-      updated_scientificName$updated_backbone_scientificName[which(backbone_scientificName == updated_scientificName$backbone_scientificName)],
+      updated_scientificName$updated_backbone_scientificName[
+        which(backbone_scientificName == 
+                updated_scientificName$backbone_scientificName)],
       backbone_scientificName)) %>% ungroup()
   
-  # update accepted of verified synonyms
-  verified_synonyms <- verified_synonyms %>% 
+  # update acceptedName of verified taxa
+  verified_taxa <- verified_taxa %>% 
     rowwise() %>%
-    mutate(backbone_accepted = ifelse(
-      backbone_accepted %in% updated_accepted$backbone_accepted,
-      updated_accepted$updated_backbone_accepted[which(backbone_accepted == updated_accepted$backbone_accepted)],
-      backbone_accepted)) %>% ungroup()
+    mutate(backbone_acceptedName = ifelse(
+      backbone_acceptedName %in% updated_acceptedName$backbone_acceptedName,
+      updated_acceptedName$updated_backbone_acceptedName[
+        which(backbone_acceptedName == 
+                updated_acceptedName$backbone_acceptedName)],
+      backbone_acceptedName)) %>% ungroup()
 
-  # add new synonyms to verified synonyms
-  verified_synonyms <- verified_synonyms %>% bind_rows(new_synonyms)
+  # add new synonyms to verified taxa
+  verified_taxa <- verified_taxa %>% bind_rows(new_synonyms)
+  
+  # add new unmatches to verified taxa
+  verified_taxa <- verified_taxa %>% bind_rows(new_unmatches)
   
   # unused synonyms
-  unused_synonyms <- verified_synonyms %>% 
+  unused_taxa <- verified_taxa %>% 
     filter(!backbone_taxonKey %in% taxa$backbone_taxonKey)
   
-  return(list(verified_synonyms = verified_synonyms,
+  return(list(verified_taxa = verified_taxa,
               new_synonyms = new_synonyms,
-              unused_synonyms = unused_synonyms,
+              unused_taxa = unused_taxa,
               updated_scientificName = updated_scientificName,
-              updated_accepted = updated_accepted))
+              updated_acceptedName = updated_acceptedName))
 }
