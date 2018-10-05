ecf686ea834e29b84b592220e6404f722b4000ca
damianooldoni <damiano.oldoni@inbo.be>
2018-10-05 17:26:14 +0200

    Adapt tests of output of verify_taxa()

--- tests/testthat/test-output_verify_taxa.R
+++ tests/testthat/test-output_verify_taxa.R
@@ -1,14 +1,14 @@
 context("output_verify_taxa")
 
 taxa_in <- data.frame(
-  checklist_scientificName = c("Aspius aspius",
+  scientificName = c("Aspius aspius",
                                "Rana catesbeiana",
                                "Polystichum tsus-simense J.Smith",
                                "Apus apus (Linnaeus, 1758)",
                                "Begonia x semperflorens hort.",
                                "Rana catesbeiana",
                                "Spiranthes cernua (L.) Richard x S. odorata (Nuttall) Lindley", "Atyaephyra desmaresti"),
-  checklist_datasetKey = c("98940a79-2bf1-46e6-afd6-ba2e85a26f9f",
+  datasetKey = c("98940a79-2bf1-46e6-afd6-ba2e85a26f9f",
                            "e4746398-f7c4-47a1-a474-ae80a4f18e92",
                            "9ff7d317-609b-4c08-bd86-3bc404b77c42",
                            "39653f3e-8d6b-4a94-a202-859359c164c5",
@@ -16,7 +16,7 @@ taxa_in <- data.frame(
                            "b351a324-77c4-41c9-a909-f30f77268bc4",
                            "9ff7d317-609b-4c08-bd86-3bc404b77c42",
                            "289244ee-e1c1-49aa-b2d7-d379391ce265"),
-  backbone_scientificName = c("Aspius aspius (Linnaeus, 1758)",
+  bb_scientificName = c("Aspius aspius (Linnaeus, 1758)",
                               "Rana catesbeiana Shaw, 1802",
                               "Polystichum tsus-simense (Hook.) J.Sm.",
                               "Apus apus (Linnaeus, 1758)",
@@ -24,43 +24,42 @@ taxa_in <- data.frame(
                               "Rana catesbeiana Shaw, 1802",
                               NA,
                               "Atyaephyra desmarestii (Millet, 1831)"),
-  backbone_taxonKey = c(2360181, 2427092, 2651108, 5228676, NA, 2427092, NA,
-                        4309705),
-  backbone_kingdom = c("Animalia", "Animalia", "Plantae",
+  bb_key = c(2360181, 2427092, 2651108, 5228676, NA, 2427092, NA, 4309705),
+  bb_kingdom = c("Animalia", "Animalia", "Plantae",
                        "Plantae", NA, "Animalia", NA, "Animalia"),
-  backbone_taxonomicStatus = c("SYNONYM", "SYNONYM", "SYNONYM", "ACCEPTED",
+  bb_taxonomicStatus = c("SYNONYM", "SYNONYM", "SYNONYM", "ACCEPTED",
                                NA, "SYNONYM", NA, "HOMOTYPIC_SYNONYM"),
-  backbone_acceptedName = c("Leuciscus aspius (Linnaeus, 1758)",
+  bb_acceptedName = c("Leuciscus aspius (Linnaeus, 1758)",
                             "Lithobates catesbeianus (Shaw, 1802)",
                             "Polystichum luctuosum (Kunze) Moore.",
                             NA, NA,
                             "Lithobates catesbeianus (Shaw, 1802)",
                             NA,
                             "Hippolyte desmarestii Millet, 1831"),
-  backbone_acceptedKey = c(5851603, 2427091, 4046493, NA, NA, 2427091, NA,
+  bb_acceptedKey = c(5851603, 2427091, 4046493, NA, NA, 2427091, NA,
                            6454754),
-  backbone_issues = c("ORIGINAL_NAME_DERIVED", NA, "ORIGINAL_NAME_DERIVED",
+  issues = c("ORIGINAL_NAME_DERIVED", NA, "ORIGINAL_NAME_DERIVED",
                       NA, NA, NA, NA, "CONFLICTING_BASIONYM_COMBINATION"),
   stringsAsFactors = FALSE)
 
 verified_taxa_in <- data.frame(
-  checklist_scientificName = c("Rana catesbeiana",
+  scientificName = c("Rana catesbeiana",
                                "Polystichum tsus-simense J.Smith",
                                "Lemnaceae",
                                "Spiranthes cernua (L.) Richard x S. odorata (Nuttall) Lindley"),
-  backbone_scientificName = c("Rana catesbeiana Shaw, 1802",
+  bb_scientificName = c("Rana catesbeiana Shaw, 1802",
                               "Polystichum tsus-tsus-tsus (Hook.) Captain",
                               "Lemnaceae",
                               NA),
-  backbone_taxonKey = c(2427092, 2651108, 6723,NA),
-  backbone_kingdom = c("Animalia", "Plantae", "Plantae", NA),
-  backbone_taxonomicStatus = c("SYNONYM", "SYNONYM", "SYNONYM", NA),
-  backbone_acceptedName = c("Lithobates dummyus (Batman, 2018)",
+  bb_key = c(2427092, 2651108, 6723,NA),
+  bb_kingdom = c("Animalia", "Plantae", "Plantae", NA),
+  bb_taxonomicStatus = c("SYNONYM", "SYNONYM", "SYNONYM", NA),
+  bb_acceptedName = c("Lithobates dummyus (Batman, 2018)",
                             "Polystichum luctuosum (Kunze) Moore.",
                             "Araceae",
                             NA),
-  backbone_acceptedKey = c(2427091, 4046493, 6979, NA),
-  backbone_issues = c(NA_character_, NA_character_, NA_character_,
+  bb_acceptedKey = c(2427091, 4046493, 6979, NA),
+  issues = c(NA_character_, NA_character_, NA_character_,
                       NA_character_),
   verification_key = c(2427091,
                    4046493,
@@ -91,7 +90,7 @@ testthat::test_that("output type", {
   expect_true(is.data.frame(output$unused_taxa))
   expect_true(is.data.frame(output$updated_scientificName))
   expect_true(is.data.frame(output$updated_acceptedName))
-  expect_true(is.data.frame(output$updated_backbone_issues))
+  expect_true(is.data.frame(output$updated_issues))
   expect_true(is.data.frame(output$duplicates_taxa))})
 
 
@@ -100,8 +99,8 @@ output_verified_taxa <-
                                "output_verified_taxa.tsv"),
                   col_types = readr::cols(
                     date_added = readr::col_date(format = "%Y-%m-%d"),
-                    backbone_taxonKey = readr::col_number(),
-                    backbone_acceptedKey = readr::col_number(),
+                    bb_key = readr::col_number(),
+                    bb_acceptedKey = readr::col_number(),
                     verification_key = readr::col_character()),
                   na = "")
 output_new_synonyms <- 
@@ -109,8 +108,8 @@ output_new_synonyms <-
                                 "output_new_synonyms.tsv"),
                   col_types = readr::cols(
                     date_added = readr::col_date(format = "%Y-%m-%d"),
-                    backbone_taxonKey = readr::col_number(),
-                    backbone_acceptedKey = readr::col_number(),
+                    bb_key = readr::col_number(),
+                    bb_acceptedKey = readr::col_number(),
                     verification_key = readr::col_character()))
 
 output_new_unmatched_taxa <- 
@@ -118,16 +117,16 @@ output_new_unmatched_taxa <-
                                 "output_new_unmatched_taxa.tsv"),
                   col_types = readr::cols(
                     date_added = readr::col_date(format = "%Y-%m-%d"),
-                    backbone_taxonKey = readr::col_number(),
-                    backbone_acceptedKey = readr::col_number()))
+                    bb_key = readr::col_number(),
+                    bb_acceptedKey = readr::col_number()))
 
 output_unused_taxa <- 
   readr::read_tsv(file = paste0("./data_test_output_verify_taxa/",
                                 "output_unused_taxa.tsv"),
                   col_types = readr::cols(
                     date_added = readr::col_date(format = "%Y-%m-%d"),
-                    backbone_taxonKey = readr::col_number(),
-                    backbone_acceptedKey = readr::col_number(),
+                    bb_key = readr::col_number(),
+                    bb_acceptedKey = readr::col_number(),
                     verification_key = readr::col_character()))
 
 output_updated_scientificName <- 
@@ -137,19 +136,19 @@ output_updated_acceptedName <-
   readr::read_tsv(file = paste0("./data_test_output_verify_taxa/",
                                 "output_updated_acceptedName.tsv"))
 
-output_updated_backbone_issues <- 
+output_updated_issues <- 
   readr::read_tsv(file = paste0("./data_test_output_verify_taxa/",
-                                "output_updated_backbone_issues.tsv"),
+                                "output_updated_issues.tsv"),
                   col_types = readr::cols(
-                    backbone_taxonKey = readr::col_number()))
+                    bb_key = readr::col_number()))
 
 output_duplicates_taxa <- 
   readr::read_tsv(file = paste0("./data_test_output_verify_taxa/",
                                 "output_duplicates_taxa.tsv"),
                   col_types = readr::cols(
                     date_added = readr::col_date(format = "%Y-%m-%d"),
-                    backbone_taxonKey = readr::col_number(),
-                    backbone_acceptedKey = readr::col_number(),
+                    bb_key = readr::col_number(),
+                    bb_acceptedKey = readr::col_number(),
                     verification_key = readr::col_character()),
                   na = "")
 
@@ -163,8 +162,8 @@ testthat::test_that("output data.frames are correct", {
   expect_equal(output$unused_taxa, output_unused_taxa)
   expect_equal(output$updated_scientificName, output_updated_scientificName)
   expect_equal(output$updated_acceptedName, output_updated_acceptedName)
-  expect_equal(output$updated_backbone_issues, 
-               output_updated_backbone_issues)
+  expect_equal(output$updated_issues, 
+               output_updated_issues)
   expect_equal(output$duplicates_taxa, 
                output_duplicates_taxa)
   })
