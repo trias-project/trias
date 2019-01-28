6da1c5a8c8efe5d144d56f0db45f259e86274110
damianooldoni <damiano.oldoni@inbo.be>
2019-01-28 15:58:54 +0100

    Remove not used param last_observed in indicator_total_year() - improve doc

--- R/indicator_total_year.R
+++ R/indicator_total_year.R
@@ -1,23 +1,26 @@
-#' Create total invasive species indicator plot (TrIAS)
+#' Create cumulative number of alien species indicator plot.
 #'
+#' This function calculates the cumulative number of species introduced per
+#' year. To do this, a column of input dataframe containing temporal information
+#' about year of introduction is required.
 #' @param df df. Contains the data as produced by the Trias pipeline,
 #'   with minimal columns.
 #' @param start_year_plot integer. Limit to use as start year of the plot. For
 #'   scientific usage, the entire period could be relevant, but for policy
 #'   purpose, focusing on a more recent period could be required.
-#' @param x_major_scale_stepsize integer. On which year interval labels are placed on
-#'   the x axis.
-#' @param x_minor_scale_stepsize integer. On which year interval minor breaks are
+#' @param x_major_scale_stepsize integer. On which year interval labels are
 #'   placed on the x axis.
-#' @param facet_column NULL or character. The column to use to create additional facet
-#'   wrap plots underneath the main graph. When NULL, no facet graph is
-#'   included. It is typically one of the highest taxonomic ranks:
-#'   \code{kingdom}, \code{phylum}, \code{class}, \code{order}, \code{family}.
-#' @param first_observed Name of the column of \code{df} containing
+#' @param x_minor_scale_stepsize integer. On which year interval minor breaks
+#'   are placed on the x axis.
+#' @param facet_column NULL or character. Name of the column to use to create
+#'   additional facet wrap plots underneath the main graph. When NULL, no facet
+#'   graph is included. It is typically one of the highest taxonomic ranks, e.g.
+#'   \code{"kingdom"}, \code{"phylum"}, \code{"class"}, \code{"order"},
+#'   \code{"family"}. Other typical breakwdowns could be geographically related,
+#'   e.g. \code{"country"}, \code{"locality"}, or \code{"pathway"} of
+#'   introduction.
+#' @param first_observed character. Name of the column of \code{df} containing
 #'   information about year of introduction. Default: \code{"first_observed"}.
-#' @param last_observed Name of the column of \code{df} containing information
-#'   about last year alien species were observed. Default:
-#'   \code{"last_observed"}.
 #'   
 #' @return ggplot2 object
 #'
@@ -43,52 +46,48 @@
 #' indicator_total_year(data, start_year_plot, x_major_scale_stepsize)
 #' # with facets
 #' indicator_total_year(data, start_year_plot, facet_column = "phylum")
-#' # specify name of column containing year of first observed and last observed
-#' indicator_total_year(data, 
-#'   first_observed = "first_observed",
-#'   last_observed = "last_observed"
-#' )
+#' # specify name of column containing year of introduction (first_observed)
+#' indicator_total_year(data, first_observed = "first_observed")
 #' }
 indicator_total_year <- function(df, start_year_plot = 1940, 
                                  x_major_scale_stepsize = 10,
                                  x_minor_scale_stepsize = 5,
                                  facet_column = NULL,
-                                 first_observed = "first_observed",
-                                 last_observed = "last_observed") {
+                                 first_observed = "first_observed") {
   
   # initial input checks
   assert_that(is.data.frame(df))
   assert_that(x_major_scale_stepsize >= x_minor_scale_stepsize)
-  assert_colnames(df, c(first_observed, first_observed), only_colnames = FALSE)
+  assert_colnames(df, first_observed, only_colnames = FALSE)
+  if (is.character(facet_column)) {
+    assert_colnames(df, facet_column, only_colnames = FALSE)
+  }
   
   # rename to default column name
   df <- 
     df %>%
-    rename_at(vars(first_observed), ~ "first_observed",
-              vars(last_observed), ~ "last_observed"
-    )
+    rename_at(vars(first_observed), ~ "first_observed")
   
   if (is.null(facet_column)) {
-    df_cleaned <- df %>%
+    df_cleaned <- 
+      df %>%
       distinct_("speciesKey", "first_observed")
   } else {
-    # check for valid facet options
-    valid_facet_options <- c("family", "order", "class", "phylum", 
-                             "kingdom", "pathway_level1", "pathway_level2",
-                             "locality", "`native range`")
-    facet_column <- match.arg(facet_column, valid_facet_options) 
-    
-    df_cleaned <- df %>%
+    df_cleaned <- 
+      df %>%
       distinct_("speciesKey", "first_observed", facet_column)
   }
   
-  # Provide warning messages for last_observed/first_observed NA values
+  # Provide warning messages for first_observed NA values
   if (nrow(filter(df, is.na(first_observed)) > 0)) {
-    warning("Some records have no first_observed ",
-            "and are not taken into account.")
-  }        
+    warning(paste0("Some records have no information about year of introduction (empty values in column ", 
+                   first_observed,
+                   ") and are not taken into account.")
+    )
+  }
   
-  df_cleaned <- df_cleaned %>%
+  df_cleaned <- 
+    df_cleaned %>%
     filter(!is.na(first_observed))  # ignore information without first_observed
   
   # Make individual records for each year up to now   
@@ -112,7 +111,8 @@ indicator_total_year <- function(df, start_year_plot = 1940,
     return(top_graph)
   } else {
     # calculate numbers
-    counts_ias_grouped <- df_extended %>%
+    counts_ias_grouped <- 
+      df_extended %>%
       group_by_("year", facet_column)  %>%
       count() %>%
       ungroup()
