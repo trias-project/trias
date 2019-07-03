3d47cfa35a8f36f261273badcf654486ec2e43e3
damianooldoni <damiano.oldoni@inbo.be>
2019-07-03 17:33:24 +0200

    Start tests of output get_table_pathways

---
+++ tests/testthat/test-output_get_table_pathways.R
@@ -0,0 +1,118 @@
+input_test_df <- data.frame(
+  key = c(152543101, 152543102, 152543110, 152543115, 152543120),
+  canonicalName = c("Gyrodactylus proterorhini", 
+                    "Aphanomyces astaci", 
+                    "Scutigera coleoptrata",
+                    "Thecaphora oxalidis", 
+                    "Tricellaria inopinata"),
+  kingdom = c("Animalia",
+              "Chromista", 
+              "Animalia", 
+              "Fungi", 
+              "Animalia"),
+  phylum = c("Platyhelminthes",
+             "Oomycota",
+             "Arthropoda",
+             "Basidiomycota",
+             "Bryozoa"),
+  first_observed = c(2011, 
+                     2018, 
+                     1830, 
+                     2014, 
+                     2011),
+  pathway_level1 = c("contaminant", 
+                     "contaminant",
+                     NA_character_,
+                     NA_character_,
+                     NA_character_),
+  pathway_level2 = c("animal_parasite",
+                     "animal_parasite",
+                     NA_character_,
+                     NA_character_,
+                     NA_character_),
+  stringsAsFactors = FALSE
+)
+# kingdom column is not the default value
+input_test_df_kingdom <- 
+  input_test_df %>%
+  rename(kingdom_species = kingdom)
+# phylum column is not the default value
+input_test_df_phylum <- 
+  input_test_df %>%
+  rename(phylum_species = phylum)
+
+testthat::test_that("Basic usage: default values", {
+  # Output basic usage : default values for all params
+  output_test_df_basic <- data.frame(
+    pathway_level1 = c("contaminant", "unknown"),
+    pathway_level2 = c("animal_parasite", ""),
+    n = as.integer(c(2, 3)),
+    examples = c(
+      "Aphanomyces astaci, Gyrodactylus proterorhini",
+      "Thecaphora oxalidis, Tricellaria inopinata, Scutigera coleoptrata"),
+    stringsAsFactors = FALSE
+  )
+  pathways_default <- get_table_pathways(input_test_df)
+  # same cols
+  expect_true(all(names(pathways_default) == names(output_test_df_basic)))
+  # same number of rows (pathways combinations)
+  expect_true(nrow(pathways_default) == nrow(output_test_df_basic))
+  # same content of dfs. No examples take into account as they are randomly
+  # selected
+  expect_equal(
+    pathways_default %>% select(-examples),
+    output_test_df_basic %>% select(-examples)
+  )
+})
+
+testthat::test_that("Use with category", {
+  output_test_df_animals <- data.frame(
+    pathway_level1 = c("contaminant", "unknown"),
+    pathway_level2 = c("animal_parasite", ""),
+    n = as.integer(c(1, 2)),
+    examples = c(
+      "Gyrodactylus proterorhini",
+      "Scutigera coleoptrata, Tricellaria inopinata"),
+    stringsAsFactors = FALSE
+  )
+  
+  pathways_plants <- get_table_pathways(input_test_df, category = "Plantae")
+  pathways_animals <- get_table_pathways(input_test_df, category = "Animalia")
+  pathways_animals_2 <- get_table_pathways(input_test_df_kingdom, 
+                                           category = "Animalia",
+                                           kingdom = "kingdom_species")
+  pathways_chordata <- get_table_pathways(input_test_df, category = "Chordata")
+  pathways_not_chordata <- get_table_pathways(input_test_df, 
+                                              category = "Not Chordata")
+  pathways_not_chordata_2 <- get_table_pathways(input_test_df_phylum, 
+                                              category = "Not Chordata",
+                                              phylum = "phylum_species")
+  expect_equal(nrow(pathways_plants), 0)
+  expect_equal(nrow(pathways_animals), 2)
+  expect_true(is.character(pathways_plants$pathway_level1))
+  expect_true(is.character(pathways_plants$pathway_level2))
+  expect_true(is.character(pathways_plants$examples))
+  expect_true(is.integer(pathways_plants$n))
+  expect_true(all(class(pathways_plants) == class(pathways_animals)))
+  expect_true(all(names(pathways_plants) == names(pathways_animals)))
+  expect_equal(
+    pathways_animals %>% select(-examples),
+    output_test_df_animals %>% select(-examples)
+  )
+  # All animals are Not Chordata
+  expect_equal(
+    pathways_not_chordata %>% select(-examples),
+    pathways_animals %>% select(-examples)
+  )
+  # pathway Chordata is empty as well
+  expect_equal(pathways_chordata, pathways_plants)
+  # same result if kingdom col has different name than default
+  expect_equal(
+    pathways_animals %>% select(-examples), 
+    pathways_animals_2 %>% select(-examples))
+  # same result if phylum col has different name than default
+  expect_equal(
+    pathways_not_chordata %>% select(-examples), 
+    pathways_not_chordata_2 %>% select(-examples))
+})
+
