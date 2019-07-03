5c71527a39447c977de0b754156a814a58e37a08
damianooldoni <damiano.oldoni@inbo.be>
2019-07-03 19:22:43 +0200

    Add mutate_if to exports

--- R/get_table_pathways.R
+++ R/get_table_pathways.R
@@ -28,8 +28,8 @@
 #'
 #' @return a data.frame
 #' @export
-#' @importFrom dplyr %>% filter distinct mutate group_by count ungroup rowwise
-#'   sample_n pull select rename_at
+#' @importFrom dplyr %>% filter distinct mutate mutate_if group_by count ungroup
+#'   rowwise sample_n pull select rename_at
 #' @importFrom assertthat assert_that
 #' @importFrom assertable assert_colnames
 #' @importFrom stringr str_c
