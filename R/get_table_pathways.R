727808b03669c623c5de37c2283b4b6cb33b4156
damianooldoni <damiano.oldoni@inbo.be>
2020-04-07 23:43:33 +0200

    Set .data as import from dplyr in get_table_pathways too

--- R/get_table_pathways.R
+++ R/get_table_pathways.R
@@ -30,7 +30,7 @@
 #' @return a data.frame
 #' @export
 #' @importFrom dplyr %>% filter distinct mutate mutate_if group_by count ungroup
-#'   rowwise sample_n pull select rename_at as_tibble
+#'   rowwise sample_n pull select rename_at as_tibble .data
 #' @importFrom assertthat assert_that
 #' @importFrom assertable assert_colnames
 #' @importFrom stringr str_c
