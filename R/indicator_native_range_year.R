29bc26033fadea72c6075204c6b5aff6e47aa15d
damianooldoni <damiano.oldoni@inbo.be>
2020-09-08 14:58:56 +0200

    Correct typo form -> from

--- R/indicator_native_range_year.R
+++ R/indicator_native_range_year.R
@@ -26,7 +26,7 @@
 #' @export indicator_native_range_year
 #' @importFrom reshape2 melt
 #' @importFrom ggplot2 ggplot geom_bar scale_y_continuous
-#' @importForm plotly ggplotly layout
+#' @importFrom plotly ggplotly layout
 #' @importFrom scales percent_format
 #' @importFrom dplyr mutate group_by case_when
 #' @importFrom magrittr %>%
