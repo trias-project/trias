11652f8a21e7737462e97723a4e058f68ea1c28d
damianooldoni <damiano.oldoni@inbo.be>
2020-09-10 18:05:59 +0200

    Remove test new function as it is not a test

--- tests/testthat/test_indicator_native_range_year.R
+++
@@ -1,7 +0,0 @@
-library(readr)
-data_input_checklist_indicators <- read_delim("https://raw.githubusercontent.com/trias-project/indicators/master/data/interim/data_input_checklist_indicators.tsv", 
-                                              "\t", escape_double = FALSE, trim_ws = TRUE)
-
-source("./R/indicator_native_range_year.r")
-
-countYearNativerange(data_input_checklist_indicators, jaartallen = c(1990:2019), type = "native_range", relative = FALSE)
