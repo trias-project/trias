cb57365e9c6575694a43f34abf22fcbcf2f249db
damianooldoni <damiano.oldoni@inbo.be>
2020-09-14 12:44:52 +0200

    Add coumentation for data, years and relative

--- R/indicator_native_range_year.R
+++ R/indicator_native_range_year.R
@@ -4,11 +4,16 @@
 #' Based on
 #' [countYearProvince][https://github.com/inbo/reporting-rshiny-grofwildjacht/blob/exoten/reporting-grofwild/R/countYearProvince.R]
 #' plot from reporting - rshiny - grofwildjacht
+#' @param data input data.frame
+#' @param years (numeric) vector years we are interested to. If \code{NULL}
+#'   (default) all years from minimum and maximum of years of first observation
+#'   are taken into account.
 #' @param type character, native_range level of interest should be one of
 #'   \code{c("native_continent", "native_range")}
 #' @param xlab character string, label of the x-axis. Default: "year".
 #' @param ylab character string, label of the y-axis. Default: "number of alien
 #'   species".
+#' @param relative (logical) if TRUE, each bar is standardised before stacking
 #'
 #' @return list with: \itemize{ \item{'static_plot': }{ggplot object, for a
 #'   given species the observed number per year and per native range is plotted
@@ -36,6 +41,11 @@ indicator_native_range_year <- function(data, years = NULL,
                                         relative = FALSE) {
   type <- match.arg(type)
 
+  # Rename to default column name
+  data <-
+    data %>%
+    rename_at(vars(first_observed), ~"first_observed")
+  
   if (is.null(years)) {
     years <- sort(unique(data$first_observed))
   }
@@ -63,10 +73,10 @@ indicator_native_range_year <- function(data, years = NULL,
 
   summaryData <- melt(table(plotData), id.vars = "first_observed")
   summaryData <- summaryData %>%
-    group_by(first_observed) %>%
+    group_by(.data$first_observed) %>%
     mutate(
-      total = sum(value),
-      perc = round((value / total) * 100, 2)
+      total = sum(.data$value),
+      perc = round((.data$value / .data$total) * 100, 2)
     )
 
   # Summarize data per year
@@ -90,7 +100,10 @@ indicator_native_range_year <- function(data, years = NULL,
     text <- paste0(summaryData$location, "<br>", summaryData$value)
   }
 
-  pl <- ggplot(data = summaryData, aes(x = first_observed, y = value, fill = location, text = text)) +
+  pl <- ggplot(data = summaryData, aes(x = .data$first_observed,
+                                       y = .data$value,
+                                       fill = .data$location,
+                                       text = text)) +
     geom_bar(position = position, stat = "identity")
 
   if (relative == TRUE) {
