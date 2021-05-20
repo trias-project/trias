357e1febf70e0e8db5283199f1e398789d9689d5
Sander Devisscher <sander.devisscher@inbo.be>
2021-05-20 06:58:20 +0200

    Create climate_match.R
    #73

---
+++ R/climate_match.R
@@ -0,0 +1,18 @@
+climate_match <- function(region, 
+                          species, 
+                          scenario = "all",
+                          n_totaal,
+                          perc_climate,
+                          coord_unc, 
+                          BasisOfRecord){
+  
+  # Setup ####
+  # Checks ####
+  # Download data ####
+  # Climate matching occurence data ####
+  # Determine future scenarios ####
+  # Per scenario filter ####
+  # Thresholds ####
+  # Return ####
+  
+}
