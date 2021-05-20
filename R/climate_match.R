f4e5ec03f37b012e8145d22783980e37dc2a0e64
Sander Devisscher <sander.devisscher@inbo.be>
2021-05-20 07:21:48 +0200

    Add region checks
    #73

--- R/climate_match.R
+++ R/climate_match.R
@@ -15,6 +15,23 @@ climate_match <- function(region,
   library(tidyverse)
   
   # Checks ####
+  ## Region ##
+  if(is.na(region)){
+    stop("region is missing")
+  }else{
+    region <- tolower(region)
+    
+    worldmap <- getMap(resolution = "low")
+    
+    valid_regions <- tolower(unique(worldmap$REGION))
+    
+    if(region %in% valid_regions){
+      region_shape <- subset(worldmap, tolower(worldmap$REGION) == region) 
+    }else{
+      stop("the provided region is not valid")
+    }
+  }
+  
   # Download data ####
   # Climate matching occurence data ####
   # Determine future scenarios ####
