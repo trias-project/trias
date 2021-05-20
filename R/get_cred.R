ed18ae35278d8ed6bf8b4bd563eec9c1b7d16fb4
Sander Devisscher <sander.devisscher@inbo.be>
2021-05-20 10:54:05 +0200

    Make get_cred permanent
    #73

--- R/get_cred.R
+++ R/get_cred.R
@@ -6,7 +6,7 @@ get_cred <- function(x){
   if(cred == ""){
     input <- dlgInput(paste0("What is your ", x, "?"))
     cred <- input$res
-    Sys.setenv(x = cred)
+    do.call(Sys.setenv, as.list(setNames(cred, x)))
   }
   return(cred)
 }
