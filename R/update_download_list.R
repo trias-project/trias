a6ff94024100b5ae1814205d5decacad38ff58f9
Damiano Oldoni <damiano.oldoni@inbo.be>
2021-08-21 14:08:26 +0200

    Remove unneeded pkgs names in update_download_list()

--- R/update_download_list.R
+++ R/update_download_list.R
@@ -33,18 +33,18 @@
 #' @importFrom stringr str_c
 update_download_list <- function(file, download_to_add, input_checklist,
                                  url_doi_base = "https://doi.org/") {
-  downloads <- readr::read_tsv(file,
+  downloads <- read_tsv(file,
     trim_ws = TRUE,
     na = ""
   )
   # downloadKey not present
   if (is.element(download_to_add, downloads$gbif_download_key) == FALSE) {
-    metadata <- rgbif::occ_download_meta(key = download_to_add)
+    metadata <- occ_download_meta(key = download_to_add)
     gbif_download_status <- metadata$status
-    gbif_download_doi <- stringr::str_c(url_doi_base, metadata$doi)
+    gbif_download_doi <- str_c(url_doi_base, metadata$doi)
     print(gbif_download_doi)
     gbif_download_created <- metadata$created
-    readr::write_tsv(
+    write_tsv(
       x = as.data.frame(list(
         gbif_download_key = toString(download_to_add),
         input_checklist = input_checklist,
@@ -63,7 +63,7 @@ update_download_list <- function(file, download_to_add, input_checklist,
       "added to", file, "; download status =", gbif_download_status
     ))
     # reload file
-    downloads <- readr::read_tsv(file, na = "")
+    downloads <- read_tsv(file, na = "")
   } else {
     print(paste("gbif_download_Key", download_to_add, "already present in", file))
   }
@@ -71,7 +71,7 @@ update_download_list <- function(file, download_to_add, input_checklist,
   changes <- FALSE
   for (i in 1:nrow(downloads)) {
     if (downloads$gbif_download_status[i] %in% c("RUNNING", "PREPARING")) {
-      metadata <- rgbif::occ_download_meta(key = downloads$gbif_download_key[i])
+      metadata <- occ_download_meta(key = downloads$gbif_download_key[i])
       gbif_download_status <- metadata$status
       # status is SUCCEEDED or FAILED
       if (gbif_download_status %in% c("SUCCEEDED", "FAILED")) {
@@ -92,7 +92,7 @@ update_download_list <- function(file, download_to_add, input_checklist,
   if (!changes) {
     print("No changes in download status detected")
   } else {
-    readr::write_tsv(
+    write_tsv(
       x = downloads,
       file = file,
       quote_escape = FALSE
