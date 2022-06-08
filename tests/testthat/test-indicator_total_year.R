58a74c035a280c186f0466023cd43e319dbfcccc
Damiano Oldoni <damiano.oldoni@inbo.be>
2022-06-08 12:33:47 +0200

    Update tests for total_year fun conformed to #84

--- tests/testthat/test-indicator_total_year.R
+++ tests/testthat/test-indicator_total_year.R
@@ -24,20 +24,20 @@ plot_output_with_facets <-
   )
 
 testthat::test_that("Param: df", {
-  expect_error(
+  testthat::expect_error(
     indicator_total_year(3),
     "df is not a data frame"
   )
 })
 
 testthat::test_that("Param: start_year_plot", {
-  expect_error(
+  testthat::expect_error(
     indicator_total_year(input_test_df,
       start_year_plot = "1950"
     ),
     "Argument start_year_plot has to be a number."
   )
-  expect_error(
+  testthat::expect_error(
     indicator_total_year(input_test_df,
       start_year_plot = 2900
     ),
@@ -49,19 +49,19 @@ testthat::test_that("Param: start_year_plot", {
 })
 
 testthat::test_that("Param: x_scale_stepsize", {
-  expect_error(
+  testthat::expect_error(
     indicator_total_year(input_test_df,
       x_minor_scale_stepsize = "10"
     ),
     "Argument x_minor_scale_stepsize has to be a number."
   )
-  expect_error(
+  testthat::expect_error(
     indicator_total_year(input_test_df,
       x_major_scale_stepsize = "10"
     ),
     "Argument x_major_scale_stepsize has to be a number."
   )
-  expect_error(
+  testthat::expect_error(
     indicator_total_year(input_test_df,
       x_major_scale_stepsize = 50,
       x_minor_scale_stepsize = 100
@@ -74,11 +74,11 @@ testthat::test_that("Param: x_scale_stepsize", {
 })
 
 testthat::test_that("Param: facet_column", {
-  expect_error(
+  testthat::expect_error(
     indicator_total_year(input_test_df, facet_column = 3),
     "Argument facet_column has to be NULL or a character."
   )
-  expect_error(
+  testthat::expect_error(
     indicator_total_year(input_test_df,
       facet_column = "not nice"
     )
@@ -86,17 +86,17 @@ testthat::test_that("Param: facet_column", {
 })
 
 testthat::test_that("Param: taxon_key_col", {
-  expect_error(
+  testthat::expect_error(
     indicator_total_year(input_test_df, taxon_key_col = "blablabla")
   )
 })
 
 testthat::test_that("Param: first_observed", {
-  expect_error(
+  testthat::expect_error(
     indicator_total_year(input_test_df, first_observed = 4),
     "Argument first_observed has to be a character."
   )
-  expect_error(
+  testthat::expect_error(
     indicator_total_year(input_test_df,
       first_observed = "strange_colname"
     )
@@ -104,18 +104,18 @@ testthat::test_that("Param: first_observed", {
 })
 
 testthat::test_that("Param: labels", {
-  expect_error(
+  testthat::expect_error(
     indicator_total_year(input_test_df, x_lab = input_test_df),
     "Argument x_lab has to be a character or NULL."
   )
-  expect_error(
+  testthat::expect_error(
     indicator_total_year(input_test_df, y_lab = 4),
     "Argument y_lab has to be a character or NULL."
   )
 })
 
 testthat::test_that("Test warnings", {
-  expect_warning(indicator_total_year(input_test_df_with_na),
+  testthat::expect_warning(indicator_total_year(input_test_df_with_na),
     paste0(
       nrow_no_first_obs,
       " records have no information about year of introduction ",
@@ -127,8 +127,36 @@ testthat::test_that("Test warnings", {
 })
 
 testthat::test_that("Test output class", {
-  expect_type(plot_output_without_facets, type = "list")
-  expect_type(plot_output_with_facets, type = "list")
-  expect_s3_class(plot_output_without_facets, class = "gg")
-  expect_s3_class(plot_output_with_facets, class = "egg")
+  # output is a list
+  testthat::expect_type(plot_output_without_facets, type = "list")
+  testthat::expect_type(plot_output_with_facets, type = "list")
+  
+  # plot slot is a list with gg as class
+  testthat::expect_type(plot_output_without_facets$plot, type = "list")
+  testthat::expect_type(plot_output_with_facets$plot, type = "list")
+  testthat::expect_s3_class(plot_output_without_facets$plot, class = "gg")
+  testthat::expect_s3_class(plot_output_with_facets$plot, class = "egg")
+  
+  # data_top_graph is a data.frame (tibble)
+  testthat::expect_type(plot_output_with_facets$data_top_graph, type = "list")
+  testthat::expect_s3_class(plot_output_with_facets$data_top_graph,
+                            class = "data.frame")
+  testthat::expect_s3_class(plot_output_with_facets$data_top_graph,
+                            class = "tbl_df")
+  testthat::expect_type(plot_output_without_facets$data_top_graph,
+                        type = "list")
+  testthat::expect_s3_class(plot_output_without_facets$data_top_graph,
+                            class = "data.frame")
+  testthat::expect_s3_class(plot_output_without_facets$data_top_graph,
+                            class = "tbl_df")
+  
+  # data_facet_graph is NULL if faceting is activated
+  testthat::expect_null(plot_output_without_facets$date_facet_graph)
+  
+  # data_facet_graph is a data.frame (tibble) if faceting is activated
+  testthat::expect_type(plot_output_with_facets$data_facet_graph, type = "list")
+  testthat::expect_s3_class(plot_output_with_facets$data_facet_graph,
+                            class = "data.frame")
+  testthat::expect_s3_class(plot_output_with_facets$data_facet_graph,
+                            class = "tbl_df")
 })
