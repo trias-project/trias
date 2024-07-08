eb3123aa8a3ce60d8c1940ed8deeca9e4b3026da
PietrH <pieterhuy@gmail.com>
2024-07-08 15:04:11 +0200

    reduce to 80 cols

--- R/apply_decision_rules.R
+++ R/apply_decision_rules.R
@@ -197,7 +197,9 @@ apply_decision_rules <- function(df,
     ))
     df <-
       df %>%
-      dplyr::filter(!(!!rlang::sym(taxonKey)) %in% taxa_eval_out_of_min_max[[taxonKey]])
+      dplyr::filter(
+        !(!!rlang::sym(taxonKey)) %in% taxa_eval_out_of_min_max[[taxonKey]]
+      )
   }
 
   # Cut time series up to eval_year
@@ -221,9 +223,11 @@ apply_decision_rules <- function(df,
   dr_2 <-
     df %>%
     dplyr::group_by(!!rlang::sym(taxonKey)) %>%
-    dplyr::mutate(last_occ = ifelse(!!rlang::sym(year) == max(!!rlang::sym(year)),
-      !!rlang::sym(y_var), -1
-    )) %>%
+    dplyr::mutate(
+      last_occ = ifelse(!!rlang::sym(year) == max(!!rlang::sym(year)),
+                        !!rlang::sym(y_var),
+                        -1)
+      ) %>%
     dplyr::summarize(
       median_occ = stats::median(!!rlang::sym(y_var)),
       last_occ = max(.data$last_occ)
@@ -245,11 +249,12 @@ apply_decision_rules <- function(df,
     df %>%
     dplyr::group_by(!!rlang::sym(taxonKey)) %>%
     dplyr::summarize(max_occ = max(!!rlang::sym(y_var))) %>%
-    dplyr::inner_join(df %>%
-      dplyr::filter(!!rlang::sym(year) == max(!!rlang::sym(year))) %>%
-      dplyr::ungroup() %>%
-      dplyr::rename(last_value = !!rlang::sym(y_var)),
-    by = taxonKey
+    dplyr::inner_join(
+      df %>%
+        dplyr::filter(!!rlang::sym(year) == max(!!rlang::sym(year))) %>%
+        dplyr::ungroup() %>%
+        dplyr::rename(last_value = !!rlang::sym(y_var)),
+      by = taxonKey
     ) %>%
     dplyr::mutate(dr_4 = .data$last_value == .data$max_occ) %>%
     dplyr::select(!!rlang::sym(taxonKey), "dr_4")
