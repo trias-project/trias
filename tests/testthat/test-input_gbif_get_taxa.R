62f458cf07eec15725d64423175f071c22a5dc49
Damiano Oldoni <damiano.oldoni@inbo.be>
2025-07-15 14:27:50 +0200

    Skip some tests on windows based on species API

--- tests/testthat/test-input_gbif_get_taxa.R
+++ tests/testthat/test-input_gbif_get_taxa.R
@@ -2,6 +2,7 @@ context("input_gbif_get_taxa")
 
 
 testthat::test_that("taxon_keys and checklist_keys cannot be both not NULL", {
+  skip_on_os(os = "windows")
   testthat::expect_error(
     gbif_get_taxa(
       taxon_keys = 3,
--- tests/testthat/test-output_gbif_has_distribution.R
+++ tests/testthat/test-output_gbif_has_distribution.R
@@ -1,67 +1,65 @@
 context("output_gbif_has_distribution")
 
 testthat::test_that("gbif_has_distribution with user parameters", {
+  skip_on_os(os = "windows")
   testthat::expect_true(gbif_has_distribution(140563025, country = "BE"))
   testthat::expect_false(gbif_has_distribution(113794849, country = "BE", status = "DOUBTFUL"))
   testthat::expect_true(gbif_has_distribution(
     taxon_key = 140563025,
     country = "BE",
-    # uncomment after https://github.com/gbif/gbif-api/issues/94 is solved
-    # status = "PRESENT",
     establishmentMeans = "introduced"
   ))
   testthat::expect_true(gbif_has_distribution(100022263,
-    country = c("GL", "CA")
-    # uncomment after https://github.com/gbif/gbif-api/issues/94 is solved
-    # status = c(
-    #   "PRESENT",
-    #   "DOUBTFUL"
-    # )
+    country = c("GL", "CA"),
+    status = c(
+      "PRESENT",
+      "DOUBTFUL"
+    )
   ))
   testthat::expect_true(gbif_has_distribution(
     taxon_key = 100022263,
     establishmentMeans = c("introduced", "original"),
-    country = "CA"
-    # uncomment after https://github.com/gbif/gbif-api/issues/94 is solved
-    # status = c("PRESENT", "ABSENT")
+    country = "CA",
+    status = c("PRESENT", "ABSENT")
   ))
 })
 
 testthat::test_that("gbif_has_distribution without user parameters", {
+  skip_on_os(os = "windows")
   testthat::expect_true(gbif_has_distribution(2225776))
   testthat::expect_false(gbif_has_distribution(121483688))
 })
 
 testthat::test_that("gbif_has_distribution with multiple distributions", {
+  skip_on_os(os = "windows")
   testthat::expect_true(gbif_has_distribution(139334288, country = "CA"))
-  # uncomment after https://github.com/gbif/gbif-api/issues/94 is solved
-  # testthat::expect_true(gbif_has_distribution(139334288,
-  #   country = "CA",
-  #   status = "present"
-  # ))
-  # uncomment after https://github.com/gbif/gbif-api/issues/94 is solved
-  # testthat::expect_true(gbif_has_distribution(139334288,
-  #   country = "CA",
-  #   status = "absent"
-  # ))
   testthat::expect_true(gbif_has_distribution(139334288,
     country = "CA",
-    # uncomment after https://github.com/gbif/gbif-api/issues/94 is solved
-    # status = "present",
+    status = "present"
+  ))
+  testthat::expect_true(gbif_has_distribution(139334288,
+    country = "CA",
+    status = "absent"
+  ))
+  testthat::expect_true(gbif_has_distribution(139334288,
+    country = "CA",
+    status = "present",
     establishmentMeans = "native"
   ))
   testthat::expect_false(gbif_has_distribution(139334288,
     country = "FR", # instead of "CA"
-    # uncomment after https://github.com/gbif/gbif-api/issues/94 is solved
-    # status = "present",
+    status = "present",
     establishmentMeans = "native"
   ))
 })
 
 testthat::test_that(
   "gbif_has_distribution is case insensitive", {
-  testthat::expect_true(gbif_has_distribution(140563025,
-                                    country = c("bE"),
-                                    establishmentMeans = "inTrODUceD"
-  ))
+    skip_on_os(os = "windows")
+    testthat::expect_true(
+      gbif_has_distribution(140563025,
+                            country = c("bE"),
+                            establishmentMeans = "inTrODUceD"
+      )
+    )
 })
