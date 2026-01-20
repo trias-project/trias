d5e7764a9355126576e30fc4072ff8ac329769b3
Damiano Oldoni <damiano.oldoni@inbo.be>
2026-01-20 15:17:14 +0100

    Fix #153

---
+++ R/get_nubkeys.R
@@ -0,0 +1,80 @@
+#' Retrieve unique GBIF Bakbone taxon keys from a species checklist
+#' 
+#' This function is a wrapper around `rgbif::name_usage()` to retrieve the (unique) GBIF
+#' Backbone taxon keys (nubKeys) from a species checklist identified by its
+#' datasetKey. It allows to choose whether to return synonyms or accepted taxa
+#' only. In this case, `rgbif::name_usage()` is used a second time to get all
+#' the taxonomic information of the accepted taxa the synonyms are pointing to within the GBIF Backbone. Synonym relationships within the checklist itself are not taken into account.
+#' @param datasetKey (character) Unique identifier of a GBIF species checklist.
+#' @param allow_synonyms (logical) Default: TRUE. If `FALSE`, the accepted taxa
+#'   the synonyms refers to are returned instead of the synonyms themselves.
+#' @return A (unique) vector of GBIF Backbone taxon keys (nubKeys). If
+#'   `allow_synonyms` is `TRUE`, the keys are retrieved from
+#'   `rgbif::name_usage()$data` directly. If `allow_synonyms` is `FALSE`, the
+#'   accepted taxa keys are retrieved by calling `rgbif::name_usage()` for each
+#'   synonym key obtained from
+#'   `rgbif::name_usage()$data` and extracting the accepted taxa keys from 
+#'   them. The final keys are returned as unique values.
+#' @family checklist functions
+#' @export
+#' @importFrom dplyr %>% .data
+#' @examples
+#' dataset_key <- "e082b10e-476f-43c1-aa61-f8d92f33029a"
+#' # Retrieve all GBIF Backbone taxon keys: synonyms are included
+#' get_nubkeys(datasetKey = dataset_key)
+#' # Retrieve accepted taxa GBIF Backbone taxon keys
+#' get_nubkeys(datasetKey = dataset_key, allow_synonyms = FALSE)
+get_nubkeys <- function(datasetKey, allow_synonyms = TRUE) {
+  # Return error if `datasetKey` is NULL
+  assertthat::assert_that(
+    !is.null(datasetKey),
+    msg = "`datasetKey` should be a single (scalar) non-NA character value."
+  )
+  # Return error if `datasetKey` is not a scalar character, no NA allowed
+  assertthat::assert_that(
+    all(purrr::is_scalar_character(datasetKey) & !is.na(datasetKey)),
+    msg = "`datasetKey` should be a single (scalar) non-NA character value."
+  )
+  # Return error if `allow_synonyms` is NULL
+  assertthat::assert_that(
+    !is.null(allow_synonyms),
+    msg = "`allow_synonyms` should be a single (scalar) non-NA logical value."
+  )
+  # Return error if `allow_synonyms` is not a scalar logical, no NA allowed
+  assertthat::assert_that(
+    all(purrr::is_scalar_logical(allow_synonyms) & !is.na(allow_synonyms)),
+    msg = "`allow_synonyms` should be a single (scalar) non-NA logical value."
+  )
+  
+  # Retrieve nubKeys from the checklist
+  df <- rgbif::name_usage(datasetKey = datasetKey, limit = 9999)$data %>%
+    dplyr::filter(.data$origin == "SOURCE")
+  # Throw a message if some taxa are not matched to GBIF Backbone (nubKey is NA)
+  # and remove them from the list of nub_keys
+  if (any(is.na(df$nubKey))) {
+    warning(paste(
+      sum(is.na(df$nubKey)),
+      "taxa from the checklist with datasetKey",
+      datasetKey,
+      "are not matched to GBIF Backbone and are ignored."
+    ))
+    df <- df %>%
+      dplyr::filter(!is.na(.data$nubKey))
+  }
+  nub_keys <- df %>%
+    dplyr::pull(.data$nubKey) %>%
+    unique()
+  if (allow_synonyms == TRUE) {
+    return(nub_keys)
+  } else {
+    nub_keys %>%
+      purrr::map(function(x) rgbif::name_usage(x)$data) %>%
+      purrr::list_rbind() %>%
+      # Choose the accepted taxa instead of synonyms
+      dplyr::mutate(
+        accepted_taxa = dplyr::coalesce(.data$acceptedKey, .data$key)
+      ) %>%
+      dplyr::pull(.data$accepted_taxa) %>%
+      unique()
+  }
+}
---
+++ tests/testthat/test-get_nubkeys.R
@@ -0,0 +1,162 @@
+context("test-get_nubkeys")
+
+# Test input validation ----
+
+test_that("datasetKey must be a scalar character", {
+  expect_error(
+    get_nubkeys(datasetKey = c("key1", "key2")),
+    "`datasetKey` should be a single \\(scalar\\) non-NA character value."
+  )
+  
+  expect_error(
+    get_nubkeys(datasetKey = 123),
+    "`datasetKey` should be a single \\(scalar\\) non-NA character value."
+  )
+  
+  expect_error(
+    get_nubkeys(datasetKey = NA_character_),
+    "`datasetKey` should be a single \\(scalar\\) non-NA character value."
+  )
+  
+  expect_error(
+    get_nubkeys(datasetKey = NULL),
+    "`datasetKey` should be a single \\(scalar\\) non-NA character value."
+  )
+})
+
+test_that("allow_synonyms must be a scalar logical", {
+  expect_error(
+    get_nubkeys(
+      datasetKey = "e082b10e-476f-43c1-aa61-f8d92f33029a",
+      allow_synonyms = c(TRUE, FALSE)
+    ),
+    "`allow_synonyms` should be a single \\(scalar\\) non-NA logical value."
+  )
+  
+  expect_error(
+    get_nubkeys(
+      datasetKey = "e082b10e-476f-43c1-aa61-f8d92f33029a",
+      allow_synonyms = "yes"
+    ),
+    "`allow_synonyms` should be a single \\(scalar\\) non-NA logical value."
+  )
+  
+  expect_error(
+    get_nubkeys(
+      datasetKey = "e082b10e-476f-43c1-aa61-f8d92f33029a",
+      allow_synonyms = NA
+    ),
+    "`allow_synonyms` should be a single \\(scalar\\) non-NA logical value."
+  )
+  
+  expect_error(
+    get_nubkeys(
+      datasetKey = "e082b10e-476f-43c1-aa61-f8d92f33029a",
+      allow_synonyms = NULL
+    ),
+    "`allow_synonyms` should be a single \\(scalar\\) non-NA logical value."
+  )
+})
+
+# Test output structure ----
+
+test_that("get_nubkeys returns a vector", {
+  skip_on_cran()
+  
+  dataset_key <- "e082b10e-476f-43c1-aa61-f8d92f33029a"
+  
+  # With synonyms
+  result_with_synonyms <- get_nubkeys(
+    datasetKey = dataset_key,
+    allow_synonyms = TRUE
+  )
+  
+  expect_true(is.vector(result_with_synonyms))
+  expect_type(result_with_synonyms, "integer")
+  
+  # Without synonyms
+  result_without_synonyms <- get_nubkeys(
+    datasetKey = dataset_key,
+    allow_synonyms = FALSE
+  )
+  
+  expect_true(is.vector(result_without_synonyms))
+  expect_type(result_without_synonyms, "integer")
+})
+
+test_that("get_nubkeys returns unique values", {
+  skip_on_cran()
+  
+  dataset_key <- "e082b10e-476f-43c1-aa61-f8d92f33029a"
+  
+  # With synonyms
+  result_with_synonyms <- get_nubkeys(
+    datasetKey = dataset_key,
+    allow_synonyms = TRUE
+  )
+  
+  expect_equal(length(result_with_synonyms), length(unique(result_with_synonyms)))
+  
+  # Without synonyms
+  result_without_synonyms <- get_nubkeys(
+    datasetKey = dataset_key,
+    allow_synonyms = FALSE
+  )
+  
+  expect_equal(
+    length(result_without_synonyms),
+    length(unique(result_without_synonyms))
+  )
+})
+
+# Test functional behavior ----
+
+test_that(
+  "allow_synonyms = FALSE returns <= keys than allow_synonyms = TRUE", {
+  skip_on_cran()
+  
+  dataset_key <- "e082b10e-476f-43c1-aa61-f8d92f33029a"
+  
+  result_with_synonyms <- get_nubkeys(
+    datasetKey = dataset_key,
+    allow_synonyms = TRUE
+  )
+  
+  result_without_synonyms <- get_nubkeys(
+    datasetKey = dataset_key,
+    allow_synonyms = FALSE
+  )
+  
+  # When synonyms are resolved to accepted taxa, there should be fewer or equal keys
+  expect_true(length(result_without_synonyms) <= length(result_with_synonyms))
+})
+
+test_that("get_nubkeys default parameter is allow_synonyms = TRUE", {
+  skip_on_cran()
+  
+  dataset_key <- "e082b10e-476f-43c1-aa61-f8d92f33029a"
+  
+  result_default <- get_nubkeys(datasetKey = dataset_key)
+  result_explicit <- get_nubkeys(
+    datasetKey = dataset_key,
+    allow_synonyms = TRUE
+  )
+  
+  expect_equal(result_default, result_explicit)
+})
+
+# Warning is returned if some taxa are not matching to GBIF Backbone
+test_that("Warning is given for taxa not matched to GBIF Backbone", {
+  skip_on_cran()
+  
+  dataset_key <- "79d65658-526c-4c78-9d24-1870d67f8439"
+  
+  expect_warning(
+    get_nubkeys(datasetKey = dataset_key),
+    regexp = paste(
+      "taxa from the checklist with datasetKey",
+       dataset_key,
+      "are not matched to GBIF Backbone and are ignored."
+    )
+  )
+})
